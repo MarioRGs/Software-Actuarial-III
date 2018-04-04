5261d971fecf7792bd1f8a6c5b5f25dab1c0d537
Mario Rodriguez <mariovni-1@hotmail.com>
2018-04-04 10:18:24 -0500

    clses

---
+++ clase020418.R
@@ -0,0 +1,28 @@
+#Scoing rules
+lm
+lm <- function(x){x*x}
+lm
+rm (lm)
+lm
+library(swirl)
+search()
+
+#ambito lexicologico
+
+hacer.potencia <- function(n){
+  potencia <- function(x){
+    x^n
+  }
+  potencia
+}
+
+y <- 10
+f <- function(x){
+  y <- 2
+  y^2 + g(x)
+}
+g <- function(x){
+  x*y
+}
+
+f(3)
